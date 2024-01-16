9f9fb40ffb67d57c13bd570e35b2a1d1c6f413ff
dkekzhs5 <dkekzhs5@naver.com>
2024-01-16 14:30:35 +0900

    [Feat] add ExceptionHandler, BaseTimeEntity (#S10P12A404-71)

---
+++ backend/src/main/java/com/ssafy/backend/common/exception/GlobalExceptionHandler.java
@@ -0,0 +1,19 @@
+package com.ssafy.backend.common.exception;
+
+import com.ssafy.backend.common.utils.HttpResponseBody;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
+
+
+    @ExceptionHandler(value = MyException.class)
+    public ResponseEntity<HttpResponseBody<?>> catchMyException(MyException e){
+        return new ResponseEntity(new HttpResponseBody<>("FAIL", e.getMessage()), HttpStatus.BAD_REQUEST);
+    }
+
+}
--- backend/src/main/java/com/ssafy/backend/common/exception/MyException.java
+++ backend/src/main/java/com/ssafy/backend/common/exception/MyException.java
@@ -2,7 +2,7 @@ package com.ssafy.backend.common.exception;
 
 import org.springframework.http.HttpStatus;
 
-public class MyException extends Exception{
+public class MyException extends RuntimeException{
     private HttpStatus status;
     public MyException(String msg, HttpStatus status){
         super(msg);
