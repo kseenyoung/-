a8c4881a0c76907aa9a82f9a22aec5908ce41e66
youngjoooh <dhdudwn95@hansung.ac.kr>
2024-01-18 00:52:26 +0900

    [Feat] Redis설정파일 추가 / Redis에 저장할 Answer엔티티 생성(#S10P11A404-86)

---
+++ backend/src/main/java/com/ssafy/backend/room/repository/AnswerRepository.java
@@ -0,0 +1,9 @@
+package com.ssafy.backend.room.repository;
+
+
+import com.ssafy.backend.room.domain.Answer;
+import org.springframework.data.repository.CrudRepository;
+
+public interface AnswerRepository extends CrudRepository<Answer, Integer> {
+
+}
