10eb1f97e522b176ce43257d3c480e0a622afbe8
dkekzhs5 <dkekzhs5@naver.com>
2024-01-17 16:49:41 +0900

    [Feat] 모꼬지 도메인 구현,비지니스 템플릿, user Entity 수정 (#S10P12A404-79)

--- backend/src/main/java/com/ssafy/backend/user/domain/User.java
+++ backend/src/main/java/com/ssafy/backend/user/domain/User.java
@@ -1,12 +1,11 @@
 package com.ssafy.backend.user.domain;
 
+import com.ssafy.backend.mokkoji.domain.Mokkoji;
 import com.ssafy.backend.user.model.UserSignupDto;
 import lombok.*;
 import org.springframework.lang.Nullable;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.Id;
+import javax.persistence.*;
 import java.time.LocalDateTime;
 import java.util.Date;
 @AllArgsConstructor
@@ -17,7 +16,11 @@ import java.util.Date;
 @Getter
 public class User {
     @Column
-    private Integer guildId, userPoint;;
+    private Integer userPoint;;
+
+    @ManyToOne
+    @JoinColumn(name = "mokkojiId")
+    private Mokkoji mokkojiId;
 
     @Column
     private String userPassword,  userName,
