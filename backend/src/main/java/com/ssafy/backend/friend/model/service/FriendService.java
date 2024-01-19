e7f5465790f298ed58f511a3ae2a1b79faabb954
kseenyoung <kseenyoung@gmail.com>
2024-01-19 09:56:25 +0900

    [Feat] 친구 목록 보기 API (#S10P12A404-26)

--- backend/src/main/java/com/ssafy/backend/friend/model/service/FriendService.java
+++ backend/src/main/java/com/ssafy/backend/friend/model/service/FriendService.java
@@ -1,18 +1,23 @@
 package com.ssafy.backend.friend.model.service;
 
 import com.ssafy.backend.common.exception.MyException;
+import com.ssafy.backend.friend.model.FriendVO;
 import com.ssafy.backend.friend.model.repository.FriendRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 
 public interface FriendService {
 
-    public void requestFriend(String userId, String userId2) throws MyException;
+    public void requestFriend(String userId, String userId2);
 
-    public void accessFriend(String accessUserId, String accessUuserId2) throws MyException;
+    public void accessFriend(String accessUserId, String accessUuserId2);
 
-    public void quitFriend(String quitUserId, String quitUserId2) throws MyException;
+    public void quitFriend(String quitUserId, String quitUserId2);
 
-    int countFriend(String countUserId) throws MyException;
+    int countFriend(String countUserId);
+
+    List<FriendVO> listFriends(String listUserId);
 }
