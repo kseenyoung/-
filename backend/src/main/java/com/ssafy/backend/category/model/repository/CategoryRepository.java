fb9485dfec5b80cad675e58af9e02ffab8255769
dkekzhs <68364199+dkekzhs@users.noreply.github.com>
2024-01-20 01:13:45 +0900

    [Refactor] 모꼬지와 카테고리 도메인 분리 (#S10P12A404-112)

--- backend/src/main/java/com/ssafy/backend/category/model/repository/CategoryRepository.java
+++ backend/src/main/java/com/ssafy/backend/category/model/repository/CategoryRepository.java
@@ -1,9 +1,7 @@
-package com.ssafy.backend.mokkoji.model.repository;
+package com.ssafy.backend.category.model.repository;
 
-import com.ssafy.backend.mokkoji.model.domain.Category;
+import com.ssafy.backend.category.model.domain.Category;
 import org.springframework.data.jpa.repository.JpaRepository;
-
-import java.util.List;
 
 public interface CategoryRepository extends JpaRepository<Category,Integer> {
 }
