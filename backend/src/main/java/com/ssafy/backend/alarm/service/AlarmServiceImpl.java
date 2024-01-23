8fac968b8be63c12254cbc845311558bd40bd980
kseenyoung <kseenyoung@gmail.com>
2024-01-23 12:18:04 +0900

    [Feat] 알람 확인하기, ë¦모든 ë¦ 알람 보기, 읽지 않은 알람보기

--- backend/src/main/java/com/ssafy/backend/alarm/service/AlarmServiceImpl.java
+++ backend/src/main/java/com/ssafy/backend/alarm/service/AlarmServiceImpl.java
@@ -1,40 +1,58 @@
 package com.ssafy.backend.alarm.service;
 
-import com.ssafy.backend.alarm.mode.dto.CheckAlarmDto;
 import com.ssafy.backend.alarm.model.domain.Alarm;
-import com.ssafy.backend.alarm.model.dto.ListOfAllAlarmDto;
-import com.ssafy.backend.alarm.model.dto.ListOfUncheckedtAlarmDto;
+import com.ssafy.backend.alarm.model.dto.CheckAlarmDto;
 import com.ssafy.backend.alarm.model.dto.ReqestAlarmDto;
 import com.ssafy.backend.alarm.model.repository.AlarmRepository;
+import com.ssafy.backend.common.exception.BaseException;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
+import static com.ssafy.backend.common.response.BaseResponseStatus.NOT_EXIST_ALARM_ID;
+
 @Service
-public class AlarmServiceImpl implements  AlarmService{
+@Slf4j
+public class AlarmServiceImpl implements  AlarmService {
 
     @Autowired
     AlarmRepository alarmRepository;
 
     @Override
     public void requestAlarm(ReqestAlarmDto reqestAlarmDto) {
+        alarmRepository.save(
+                Alarm.builder()
+                        .tagId(reqestAlarmDto.getTagId())
+                        .userId(reqestAlarmDto.getUserId())
+                        .isChecked(0)
+                        .requestedUserId(reqestAlarmDto.getRequestedUserId())
+                        .build());
     }
-
     @Override
     public void checkAlarm(CheckAlarmDto checkAlarmDto) {
+        Alarm alarm = alarmRepository.findById(checkAlarmDto.getAlarmId()).orElseThrow(() -> new BaseException(NOT_EXIST_ALARM_ID));
+        alarm.setIsChecked(1);
+
+//        log.info("Alarm id : {}", alarm.getAlarmId());
+        alarmRepository.save(alarm);
 
     }
 
     @Override
-    public List<Alarm> listofAllAlarm(ListOfAllAlarmDto listOfAllAlarmDto) {
+    public List<Alarm> listofAllAlarm(String userId) {
 
-        return null;
+        List<Alarm> allByUserId = alarmRepository.findAllByUserId(userId);
+
+        return allByUserId;
     }
 
     @Override
-    public List<Alarm> listOfUncheckedAlarm(ListOfUncheckedtAlarmDto listOfUncheckedAlarmDto) {
+    public List<Alarm> listOfUncheckedAlarm(String userId) {
 
-        return null;
+        List<Alarm> byUserIdAndIsChecked = alarmRepository.findByUserIdAndIsChecked(userId, 0);
+
+        return byUserIdAndIsChecked;
     }
 }
