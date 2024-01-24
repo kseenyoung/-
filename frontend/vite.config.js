667e59fee40f9ff4fc4c0099e01c8db0cab85317
youngjoo <johnohwb@gmail.com>
2024-01-24 14:20:22 +0900

    [Feat]vite.config.js fs 라이브러리 수정 (#S10P12A404-183)

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,7 @@
 import { fileURLToPath, URL } from 'node:url';
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
-const fs = require('fs');
+import fs from "fs";
 
 // https://vitejs.dev/config/
 export default defineConfig({
