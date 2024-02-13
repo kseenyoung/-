a3a1cc09857a3d4c321d1cc97b4e732be694d8a6
dkekzhs <68364199+dkekzhs@users.noreply.github.com>
2024-02-14 00:57:28 +0900

    [Feat] 보드 기능 전부다 구현 (#S10P12A404-330)

--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -41,28 +41,18 @@
 
       <div
         style="color: white;"
-        v-else-if="userStore.loginUserInfo.userId != null && arr.length ===0"
+        v-if="userStore.loginUserInfo.userId != null && arr.length ===0"
         >
         <div class="is-typed" style="" @click="navigateToMyPageSchedule">
           <h3 style=""> </h3>
           <p style="" class="font-weight-bold"><h3> 다각 만들러가기</h3></p>
         </div>
-        
-        <div class="friends">
-          <div class="bubble medium bottom" style="margin-left : 80%;">
-              친구 <b style="color: red;">{{ loginFriends.length }}</b> 명이 <br/> 로그인중이에요
-              <br/>
-          </div>
-            <img  src="@/assets/friends.png" @click="showFriends" style="width : 13%; margin-left : 80%; margin-bottom : 10%"/>        
-        </div>
       </div>
-
-      
-        <div
+      <div
           style="color: white;"
           v-else
           @click="navigateToStudyRoom"
->
+          >
           <div class="is-typed">
             <h3 style="display:inline-block;"> 
               <VueWriter :array="arr" style="display:inline-block;" :caret="underscore" />
@@ -70,6 +60,13 @@
         <p style="display: inline-block;" class="font-weight-bold"><h3> 공부하기</h3></p>
         </div>
       </div>
+        <div v-if="userStore.loginUserInfo.userId" class="friends">
+            <div class="bubble medium bottom" style="margin-left : 80%;">
+                친구 <b style="color: red;">{{ loginFriends.length }}</b> 명이 <br/> 로그인중이에요
+                <br/>
+            </div>
+              <img  src="@/assets/friends.png" @click="showFriends" style="width : 13%; margin-left : 80%; margin-bottom : 10%"/>        
+        </div>
       </div>
       </div>
         <div class="background">
--- frontend/src/views/posts/PostListView.vue
+++ frontend/src/views/posts/PostListView.vue
@@ -1,7 +1,14 @@
 <template>
-<div class="boardPage">
+  <div class="boardPage">
     <img src="@/assets/board.png" class="board" />
     <hr class="my-4" />
+    <input
+      type="text"
+      class="search_bar"
+      placeholder="제목을 입력하고 엔터로 검색하세요"
+      v-model="searchTitle"
+      @keydown.enter="fetchPosts(0)"
+    />
     <div class="postList">
       <div v-for="post in posts" :key="post.boardId" class="post">
         <PostItem
@@ -12,53 +19,84 @@
         ></PostItem>
       </div>
     </div>
-  
+    <div class="pagination" v-if="totalPages > 1">
+      <button
+        v-for="n in pageCount"
+        :key="n"
+        @click="goToPage(n)"
+        :class="{ active: currentPage === n }"
+      >
+        {{ n }}
+      </button>
+    </div>
     <div class="d-flex">
-    <button v-if="userStore.loginUserInfo.userId" class="post-create-btn" type="button" @click="goCreatePage">글쓰기</button>
-  </div>
+      <button
+        v-if="userStore.loginUserInfo.userId"
+        class="post-create-btn"
+        type="button"
+        @click="goCreatePage"
+      >
+        글쓰기
+      </button>
+    </div>
   </div>
 </template>
 
 <script setup>
-import { useRouter } from 'vue-router'
-import { ref } from 'vue'
-import { useBoardStore } from '@/stores/board'
-import PostItem from '@/components/posts/PostItem.vue'
-import SimpleDagak from '@/components/dagak/SimpleDagak.vue';
-import {useUserStore} from '@/stores/user';
-const router = useRouter()
-const boardStore = useBoardStore()
-const userStore = useUserStore()
+import { useRouter } from "vue-router";
+import { ref, computed } from "vue";
+import { useBoardStore } from "@/stores/board";
+import PostItem from "@/components/posts/PostItem.vue";
+import { useUserStore } from "@/stores/user";
+const router = useRouter();
+const boardStore = useBoardStore();
+const userStore = useUserStore();
 
 const goCreatePage = () => {
   router.push({
-    name: 'postCreate'
-  })
-}
-const posts = ref([])
-const fetchPosts = async () => {
-  await boardStore.getPosts();
-  console.log('총 포스트:', boardStore.posts)
-  posts.value = boardStore.posts;
-}
+    name: "postCreate",
+  });
+};
+const posts = ref([]);
+const totalPages = ref(0); // 총 페이지 수
+const currentPage = ref(1); // 현재 페이지
+const displayPage = 5; // 한 번에 보여줄 페이지 수
+const searchTitle = ref(""); // 검색어를 저장할 데이터
 
-fetchPosts()
+const fetchPosts = async (page) => {
+  await boardStore.getPosts(page, searchTitle.value);
+  console.log("총 포스트:", boardStore.posts);
+  posts.value = boardStore.posts;
+  totalPages.value = boardStore.totalPages;
+};
+
+fetchPosts(0);
 const goDetailPage = (id) => {
   router.push({
-    name: 'postDetail',
+    name: "postDetail",
     params: {
-      id
-    }
-  })
-}
+      id,
+    },
+  });
+};
+const goToPage = (page) => {
+  currentPage.value = page; // 현재 페이지 업데이트
+  fetchPosts(page - 1); // 현재 페이지에 맞는 게시글 다시 가져오기
+};
+
+const pageCount = computed(() => {
+  let start = Math.max(1, currentPage.value - Math.floor(displayPage / 2));
+  let end = Math.min(totalPages.value, start + displayPage - 1);
+  if (totalPages.value < displayPage) {
+    start = 1;
+  }
+  return Array.from({ length: end - start + 1 }, (v, i) => start + i);
+});
 
 const formatDate = (timestampArray) => {
   const date = new Date(...timestampArray);
   return date.toLocaleString();
 };
-
-
-
 </script>
 
 <style lang="scss" scoped>
@@ -67,7 +105,7 @@ const formatDate = (timestampArray) => {
   font-size: 1rem;
 }
 .boardPage {
-  background-image: url('@/assets/background.gif');
+  background-image: url("@/assets/background.gif");
   background-size: cover;
   height: 100vh;
   padding-top: 40px;
@@ -75,22 +113,21 @@ const formatDate = (timestampArray) => {
   flex-direction: column; /* 컬럼 방향으로 요소들을 정렬 */
   align-items: center; /* 수평 방향으로 중앙 정렬 */
   justify-content: center; /* 수직 방향으로 중앙 정렬 */
-
 }
 .board {
   width: 70%;
   height: 85%;
   position: absolute;
 }
-.postList{
+.postList {
   width: 80%;
   display: flex;
   flex-wrap: wrap;
   overflow-y: auto; /* 스크롤 기능을 유지합니다. */
-  height: 60%; 
+  height: 60%;
   margin-left: 20%;
 }
-.post{
+.post {
   width: 40%;
   margin: 1%;
 }
@@ -99,11 +136,17 @@ const formatDate = (timestampArray) => {
 }
 .post-create-btn {
   z-index: 1;
-  background-color: #4B4B4B; /* 칠판과 비슷한 짙은 회색 배경색 */
+  background-color: #4b4b4b; /* 칠판과 비슷한 짙은 회색 배경색 */
   color: white; /* 텍스트 색상은 흰색 */
   border: none; /* 테두리 제거 */
   padding: 10px 20px; /* 패딩으로 버튼 내부 공간 조절 */
   cursor: pointer; /* 마우스를 올렸을 때 커서 모양을 손가락 모양으로 변경 */
 }
-
+.pagination {
+  z-index: 3;
+}
+.search_bar {
+  width: 50%;
+  z-index: 1;
+}
 </style>
